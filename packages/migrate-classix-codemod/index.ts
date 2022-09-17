6fc25363c2b673fe2117b11caff4a055479e9b49
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2022-09-17 15:45:44 -0400

    Create index.ts

---
+++ packages/migrate-classix-codemod/index.ts
@@ -0,0 +1,53 @@
+const callees = ["classnames", "cn", "clsx"];
+
+export default function transformer(file, api) {
+  const j = api.jscodeshift;
+  console.log(api);
+  const root = j(file.source);
+  console.log(root);
+
+  const newImport = j.importDeclaration([j.importDefaultSpecifier(j.identifier("cx"))], j.stringLiteral("classix"));
+  let addedCx = false;
+
+  const classnamesImportDeclarations = root
+    .find(j.ImportDeclaration)
+    .filter((path) => callees.includes(path.node.source.value));
+  classnamesImportDeclarations.forEach((importStatement) => {
+    importStatement.prune();
+    if (!addedCx) {
+      root.get().node.program.body.unshift(newImport);
+      addedCx = true;
+    }
+  });
+
+  callees.forEach((callee) => {
+    const calls = root.find(j.CallExpression, {
+      callee: {
+        name: callee
+      }
+    });
+    calls.forEach((call) => {
+      console.log({ call });
+      const args = [];
+      call.value.callee.name = "cx";
+      console.log(call);
+
+      call.value.arguments.forEach((arg) => {
+        arg.properties.forEach((prop) => {
+          const key = prop.key;
+          const value = prop.value;
+          console.log({ key, value });
+          args.push({ key, value });
+        });
+      });
+      console.log({ args });
+      call.value.arguments = [];
+      args.forEach((argPair) => {
+        const exp = j.logicalExpression("&&", argPair.key, argPair.value);
+        call.value.arguments.push(exp);
+      });
+    });
+  });
+
+  return root.toSource();
+}
